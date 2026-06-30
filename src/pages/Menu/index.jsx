17abf8e50f34c4507156554c1cec2bd726806c2f
Gabriel Borges Damiani <damiani.gabri70@gmail.com>
2026-06-30 08:28:48 -0300

    Create product pages and start adding images to the menu.

---
+++ src/pages/Menu/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./menu.module.css";
+
+export const Menu = () => {
+  return (
+    <>
+      <section className={styles.hero}></section>
+    </>
+  );
+};
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,12 +3,19 @@ import { MainLayout } from "../layouts/Main";
 import { AuthLayout } from "../layouts/Auth";
 import { Register } from "../pages/Register";
 import { Login } from "../pages/Login";
+import { Menu } from "../pages/Menu";
+import { Products } from "../pages/Products";
+import { Category } from "../pages/Category";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainLayout />}></Route>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="menu" element={<Menu />}></Route>
+          <Route path="products" element={<Products />}></Route>
+          <Route path="category" element={<Category />}></Route>
+        </Route>
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="register" element={<Register />}></Route>
           <Route path="login" element={<Login />}></Route>
